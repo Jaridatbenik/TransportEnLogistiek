c152440d62a6499d5a5ae017e9899b941dc622ff
Sep <SepHorsthuis@hotmail.com>
2019-02-12 12:15:56 +0100

    +Collider-based hover-popup added. this uses 2 different scripts

--- Logi&Auto_Game/Assets/SCRIPTS/UI/HoverMessageHandler.cs
+++ Logi&Auto_Game/Assets/SCRIPTS/UI/HoverMessageHandler.cs
@@ -4,8 +4,72 @@ using UnityEngine;
 
 public class HoverMessageHandler : MonoBehaviour
 {
+    RectTransform hoverMessage;
+    RectTransform hoverMessageImage;
+    Camera cam;
     void Update()
     {
-            
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        HoverObject ho = null;
+        if (hit.transform != null)
+            ho = hit.transform.GetComponent<HoverObject>();                    
+
+        if (ho != null)
+        {
+            hoverMessage = ho.message;
+            hoverMessageImage = ho.messageImage;          
+            hoverMessage.gameObject.SetActive(true);
+        }
+        else
+        {
+            if (hoverMessage != null)
+            {
+                hoverMessage.gameObject.SetActive(false);
+                hoverMessageImage = null;
+                hoverMessage = null;
+            } 
+
+        }
+
+
+        if(hoverMessageImage)
+        {            
+            Vector3 newPos = hoverMessageImage.anchoredPosition;
+
+            //X
+            if (Input.mousePosition.x > Screen.width * 0.75f)
+            {
+                newPos.x = -Mathf.Abs(newPos.x);
+            }
+            else if (Input.mousePosition.x < Screen.width * 0.25f)
+            {
+                newPos.x = Mathf.Abs(newPos.x);
+            }
+            //Y
+            if(Input.mousePosition.y > Screen.height * 0.75f)
+            {             
+                newPos.y = -Mathf.Abs(newPos.y);
+            }
+            else if(Input.mousePosition.y < Screen.height * 0.25f)
+            {                
+                newPos.y = Mathf.Abs(newPos.y);
+            }
+
+            hoverMessageImage.anchoredPosition = newPos;
+            hoverMessage.position = Input.mousePosition;
+        }        
+
+
+
+        if (hoverMessage != null)
+        {
+            hoverMessage.position = Input.mousePosition;
+        }
+    }
+
+    void Start()
+    {
+        cam = Camera.main;
     }
 }
---
+++ Logi&Auto_Game/Assets/SCRIPTS/UI/HoverObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverObject : MonoBehaviour
+{
+    public RectTransform message;
+    [HideInInspector]
+    public RectTransform messageImage;
+
+    void Awake()
+    {
+        message.gameObject.SetActive(false);
+        messageImage = message.GetChild(0).GetComponent<RectTransform>();
+    }
+}
