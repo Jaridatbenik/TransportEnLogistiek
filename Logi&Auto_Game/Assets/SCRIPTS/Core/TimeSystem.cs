af1d922169809aaa52fd6872bac3d361a2ff2ab4
Sep <SepHorsthuis@hotmail.com>
2019-02-13 12:35:32 +0100

    +More Time functionality (workhours) +Implemented sprites

--- Logi&Auto_Game/Assets/SCRIPTS/Core/TimeSystem.cs
+++ Logi&Auto_Game/Assets/SCRIPTS/Core/TimeSystem.cs
@@ -18,38 +18,58 @@ public class TimeSystem : MonoBehaviour
     [SerializeField]TextMeshProUGUI hour_min;
     [SerializeField]
     TextMeshProUGUI day_month;
+    Color baseColor;
+    [SerializeField]Color homeHourColor = Color.red;
 
+    //work
+    Vector2 workhours = new Vector2(9, 17);
+    bool working = false;
 
-    void Update()
+    void Start()
     {
-        currentTime += Time.fixedDeltaTime * timeSpeed;           
+        baseColor = hour_min.color;    
+    }
 
-        if(currentTime >= 60)
+    void FixedUpdate()
+    {
+        AddTime();
+        SetText();
+        WorkHourDetection();
+    }
+
+    void AddTime()
+    {
+        currentTime += timeSpeed;
+
+        if (currentTime >= 60)
         {
             currentTime = 0;
             minutes++;
-        } 
-        if(minutes >= 60)
+        }
+        if (minutes >= 60)
         {
             minutes = 0;
             hours++;
         }
-        if(hours >= 24)
+        if (hours >= 24)
         {
             hours = 0;
             days++;
         }
-        if(days >= monthDayAmount[months])
+        if (days >= monthDayAmount[months])
         {
-            months++;            
+            months++;
             days = 0;
         }
-        if(months >= 12)
+        if (months >= 12)
         {
             months = 0;
             print("YEAR");
         }
+    }
 
+    void SetText()
+    {
         string s_hours = AddZero(hours.ToString());
         string s_minutes = AddZero(minutes.ToString());
         string s_days = AddZero(days.ToString());
@@ -58,12 +78,36 @@ public class TimeSystem : MonoBehaviour
 
         hour_min.text = s_hours + ":" + s_minutes;
         day_month.text = s_days + "-" + s_months;
-    }
+    }    
 
     string AddZero(string s)
     {
         if (s.Length <= 1)
             s = "0" + s;
         return s;  
+    }
+
+
+    void WorkHourDetection()
+    {
+        if(hours == 9 && !working)
+        {
+            StartWorking();
+        }
+        else if(hours == 17 && working)
+        {
+            StopWorking();
+        }
+    }
+
+    void StartWorking()
+    {
+        working = true;
+        hour_min.color = baseColor;
+    }
+    void StopWorking()
+    {
+        working = false;
+        hour_min.color = homeHourColor;
     }
 }
