689504fa89bb190de2f945a0a2c2fc866458d9f4
jaridatbenik <JariKortier@gmail.com>
2019-02-13 14:57:47 +0100

    Grid System backup YAY

---
+++ GridBuilder/Assets/Classes/Main/TileUpdateHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileUpdateHandler : MonoBehaviour
+{
+    public TileGameObject obj;
+
+    void Update()
+    {
+        obj.TileUpdate();
+    }
+
+    void FixedUpdate()
+    {
+        obj.TileFixedUpdate();
+    }
+}
